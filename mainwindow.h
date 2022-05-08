c8fd9216ed29061d6c04f1337a538f8bff0a501d
tom montbord <montbordino@gmail.com>
2022-05-08 16:28:46 +0200

    ajout de commentaire pour les procédures, finPartie()->finManche() pour plus de clarté

--- mainwindow.h
+++ mainwindow.h
@@ -26,13 +26,37 @@ private:
     QPixmap *rien;
 
 private slots:
+    /**
+     * @brief lance une nouvelle partie en effacant les données de la précédente et met la couleur bleu sur le joueur
+     */
     void lancerPartie();
+
+    /**
+     * @brief change le coup du joueur, l'affiche à l'écran et lance tourMachine()
+     */
     void jouerPierre();
     void jouerFeuille();
     void jouerCiseaux();
+
+    /**
+     * @brief met la couleur bleu sur la machine, choisis son coup au hasard et l'affiche à l'écran puis lance finPartie()
+     */
     void tourMachine();
+
+    /**
+     * @brief modifie le score et prénom du joueur cible en bleu et change ceux de l'autre joueur en noir
+     * @arg caractère qui définis la cible: 'J' pour le joueur et 'M' pour la machine
+     */
     void setBlue(char);
+
+    /**
+     * @brief definis l'image du joueur machine en fonction de son coup Chifoumi::UnCoup
+     */
     void definirImageMachine(Chifoumi::UnCoup);
-    void finPartie();
+
+    /**
+     * @brief définis le joueur gagnant la manche, met à jour les scores, les affiches à l'écran et met la couleur bleu sur le joueur humain
+     */
+    void finManche();
 };
 #endif // MAINWINDOW_H
